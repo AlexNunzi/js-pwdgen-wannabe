b2244889693f535475eda8a2435252446289482d
Alex Nunzi <alex.nunzi@hotmail.it>
2023-02-21 15:00:40 +0100

    Inizializzazione progetto. Inserimento contenuto html e inserimento codice JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+alert("Stai per generare la tua password e ti chiederemo di inserire il tuo nome, il tuo cognome ed il tuo colore preferito");
+const userName = prompt("Digita il tuo nome:");
+const userSurname = prompt("Digita il tuo cognome:");
+const choosenColor = prompt("Digita il tuo colore preferito:")
+const currentYear = "23";
+
+
+// Risoluzione classica dell'esercizio
+// const passGenerator = userName + userSurname + choosenColor + currentYear;
+// document.getElementById("pass").innerHTML = passGenerator;
+
+
+// Risoluzione dell'esercizio tramite template literals
+document.getElementById("pass").innerHTML = `${userName}${userSurname}${choosenColor}${currentYear}`;
